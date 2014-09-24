aca61778a90fe2fcaae742b46405aa4d6fd39a72
thomas tsai <thomas@biotrump.com>
2014-09-25 00:40:28 +0800

    init config.sh to repo sync openCV v4l2 pico

---
+++ config.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+echo "111"
+
+#curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+REPO=${REPO:-~/bin/repo}
+sync_flags=""
+
+repo_sync() {
+	rm -rf .repo/manifest* &&
+	$REPO init -u $GITREPO -b $BRANCH -m $1.xml $REPO_INIT_FLAGS &&
+	$REPO sync $sync_flags $REPO_SYNC_FLAGS
+	ret=$?
+	if [ "$GITREPO" = "$GIT_TEMP_REPO" ]; then
+		rm -rf $GIT_TEMP_REPO
+	fi
+	if [ $ret -ne 0 ]; then
+		echo Repo sync failed
+		exit -1
+	fi
+}
+
+case `uname` in
+"Darwin")
+	# Should also work on other BSDs
+	CORE_COUNT=`sysctl -n hw.ncpu`
+	;;
+"Linux")
+	CORE_COUNT=`grep processor /proc/cpuinfo | wc -l`
+	;;
+*)
+	echo Unsupported platform: `uname`
+	exit -1
+esac
+
+GITREPO=${GITREPO:-"https://github.com/biotrump/manifest-cv"}
+BRANCH=${BRANCH:-2.4.x}
+while [ $# -ge 1 ]; do
+	case $1 in
+	-d|-l|-f|-n|-c|-q|-j*)
+		sync_flags="$sync_flags $1"
+		if [ $1 = "-j" ]; then
+			shift
+			sync_flags+=" $1"
+		fi
+		shift
+		;;
+	--help|-h)
+		# The main case statement will give a usage message.
+		echo ---help
+		exit 1
+		break
+		;;
+	-*)
+		echo "$0: unrecognized option $1" >&2
+		exit 1
+		;;
+	*)
+		break
+		;;
+	esac
+done
+
+GIT_TEMP_REPO="tmp_manifest_repo"
+if [ -n "$2" ]; then
+	GITREPO=$GIT_TEMP_REPO
+	rm -rf $GITREPO &&
+	git init $GITREPO &&
+	cp $2 $GITREPO/$1.xml &&
+	cd $GITREPO &&
+	git add $1.xml &&
+	git commit -m "manifest" &&
+	git branch -m $BRANCH &&
+	cd ..
+fi
+echo MAKE_FLAGS=-j$((CORE_COUNT + 2)) > .tmp-config
+echo CV_DIR=$PWD/dir-cv >> .tmp-config
+#echo DEVICE_NAME=$1 >> .tmp-config
+
+#echo DEVICE=hammerhead >> .tmp-config &&
+repo_sync pico-bin
+
+if [ $? -ne 0 ]; then
+	echo Configuration failed
+	exit -1
+fi
+
+mv .tmp-config .config
+
+echo Run \|./build.sh\| to start building
+
